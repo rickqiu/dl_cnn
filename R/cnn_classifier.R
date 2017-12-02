88b2ca59dd71e86f59a4df66989a4eeda8ed93e7
Rick Qiu <qiu.rick@gmail.com>
2017-12-02 19:11:02 +0000

    save model

--- R/cnn_classifier.R
+++ R/cnn_classifier.R
@@ -5,9 +5,6 @@ require(keras)
 # Data importation
 df <- readRDS(file="input/train.rds")
 
-#df$inc_angle <- as.numeric(df$inc_angle)
-#df$inc_angle[is.na(df$inc_angle)] <- 0
-
 # Feature scaling
 normalize <- function(x) (x/sqrt(sum(x^2)))
 
@@ -37,20 +34,20 @@ v0[v0 == 0] <- 1
 v0[v0 == 2] <- 0
 
 y <- matrix(c(v0,v1),nrow=length(v0))
-
 colnames(y) <- c("class0", "class1")
 
 # Keras - Tensorflow backend (LeNet7)
 n_classes <- 2
-batch_size = 32
-epochs = 100
-kernel_size = 3 
-pool_size = 2
-conv_depth = 32
-hidden_size = 128
-drop_prob_1 = 0.24
-drop_prob_2 = 0.5
-l2_lambda = 0.00015
+batch_size <- 16
+epochs <- 100
+kernel_size <- 3 
+pool_size <- 2
+conv_depth <- 32
+hidden_size <- 64
+drop_prob_1 <- 0.2
+drop_prob_2 <- 0.5
+l2_lambda <- 1e-4
+l1_lambda <- 1e-6
     
 model <- keras_model_sequential()
 
@@ -60,40 +57,47 @@ model %>%
                   strides = c(1,1), padding = 'same', 
                   kernel_initializer='he_uniform',
                   data_format = "channels_last", dtype = "float32", input_shape = dim(X[1,,,])) %>%
-    layer_activity_regularization(l1=0.0,l2=l2_lambda) %>%
+    layer_activity_regularization(l1=l1_lambda,l2=l2_lambda) %>%
     layer_activation('relu') %>%
 
     layer_conv_2d(filters = conv_depth, kernel_size = c(kernel_size,kernel_size), 
                   strides = c(1,1), padding='same',
                   kernel_initializer='he_uniform') %>%
-    layer_activity_regularization(l1=0.0,l2=l2_lambda) %>%
+    layer_activity_regularization(l1=l1_lambda,l2=l2_lambda) %>%
     layer_activation('relu') %>%
     layer_max_pooling_2d(pool_size = c(pool_size,pool_size)) %>%
     layer_dropout(drop_prob_1) %>% 
     
     layer_conv_2d(filters = conv_depth, kernel_size = c(kernel_size, kernel_size),
                   kernel_initializer='he_uniform') %>%
-    layer_activity_regularization(l1=0.0,l2=l2_lambda) %>%
+    layer_activity_regularization(l1=l1_lambda,l2=l2_lambda) %>%
     layer_activation('relu') %>%
    
     layer_conv_2d(filters = conv_depth, kernel_size = c(kernel_size,kernel_size),
                   kernel_initializer='he_uniform') %>%
-    layer_activity_regularization(l1=0.0,l2=l2_lambda) %>%
+    layer_activity_regularization(l1=l1_lambda,l2=l2_lambda) %>%
     layer_activation('relu') %>%
     layer_max_pooling_2d(pool_size = c(pool_size,pool_size)) %>%
     layer_dropout(drop_prob_1) %>%
     
     layer_flatten() %>%
     layer_dense(hidden_size,  kernel_initializer='he_uniform') %>%
-    layer_activity_regularization(l1=0.0,l2=l2_lambda) %>%
+    layer_activity_regularization(l1=l1_lambda,l2=l2_lambda) %>%
     layer_activation('relu') %>%
     layer_dropout(drop_prob_2) %>%
     
     layer_dense(n_classes,  kernel_initializer='glorot_uniform') %>%
-    layer_activity_regularization(l1=0.0,l2=l2_lambda) %>%
+    layer_activity_regularization(l1=l1_lambda,l2=l2_lambda) %>%
     layer_activation('softmax')
 
-model %>% compile(optimizer = optimizer_rmsprop(), loss = 'categorical_crossentropy',metrics = c('accuracy'))
+opt <- optimizer_rmsprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=1e-6)
+model %>% compile(optimizer = opt, loss = 'categorical_crossentropy',metrics = c('accuracy'))
 
-history <- model %>% fit(x = X, y = y, bach_size = batch_size, epochs = epochs, validation_split = .2)
+history <- model %>% fit(x = X, y = y, batch_size = batch_size, epochs = epochs, validation_split = .15)
+history
+
+save_model_hdf5(model, filepath = "input/model2")
+
+model2 <- load_model_hdf5(filepath = "input/model2")
+history <- model2 %>% fit(x = X, y = y, batch_size = batch_size, epochs = epochs, validation_split = .15)
 history
